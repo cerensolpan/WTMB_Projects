f89645ab736f11dc6ed92d7d03ead0215e51dcb8
Ceren ŞOLPAN <73752127+cerensolpan@users.noreply.github.com>
2021-01-09 02:54:11 +0300

    Create new classes, add comment lines

--- Week_1/index.js
+++ Week_1/index.js
@@ -1,34 +1,72 @@
-//reader-book-writer
-Reader = class {
-    constructor(readerName) {
-        this.readerName = readerName;
-        this.novels = [];
+// Create User class
+User = class {
+    constructor(userName) {
+        this.userName = userName;
+        this.playlist = [];
     }
-    readNovel(novel) {
-        this.novels.push(novel);
-        console.log(this.readerName + " read " + novel.novelName + " written by " + novel.author.authorName);
+    // Method for adding songs to user playlist
+    listenedPlaylist(song) {
+        this.playlist.push(song);
+        this.playlist.forEach(printSongNamesToConsole);
     }
-
 }
-Novel = class {
-    constructor(novelName, author) {
-        this.novelName = novelName;
-        this.author = author;
-    }
+// Print each of the user playlist songs to console
+printSongNamesToConsole = song => console.log(song.songName);
 
-}
-Author = class {
-    constructor(authorName) {
-        this.authorName = authorName;
+// Create Genre Class
+Genre = class {
+    constructor(genreName) {
+        this.genreName = genreName;
+        this.songs = [];
     }
 }
 
+// Create Artist Class
+Artist = class {
+    constructor(artistName) {
+        this.artistName = artistName;
+        this.songs = [];
+    }
+}
 
-ceren = new Reader("Ceren");
-baris = new Reader("Baris");
-victor = new Author("Victor Hugo");
-ali = new Author("Sabahattin Ali");
-sefiller = new Novel("Sefiller", victor);
-icimizdekiseytan = new Novel("İcimizdeki Seytan", ali);
-ceren.readNovel(icimizdekiseytan);
-baris.readNovel(sefiller);
+// Create Song Class
+Song = class {
+    constructor(songName) {
+        this.songName = songName;
+    }
+    // Method for adding songs to genres
+    addSongToGenreList(genre) {
+        this.genre = genre;
+        genre.songs.push(this);
+    }
+    // Method for adding songs to artist song list
+    addSongToArtist(artist) {
+        this.artist = artist;
+        artist.songs.push(this);
+    }
+
+}
+
+// Create new User objects
+ceren = new User("Ceren");
+yasemin = new User("Yasemin");
+
+// Create new Song object
+lucky = new Song("Lucky");
+
+// Add song to user playlist
+ceren.listenedPlaylist(lucky);
+
+// Create new Genre object
+pop = new Genre("Pop");
+
+// Add song to genres
+lucky.addSongToGenreList(pop);
+
+//Create new Artist object
+jason = new Artist("Jason Mraz");
+
+// Add song to artist
+lucky.addSongToArtist(jason);
+
+
