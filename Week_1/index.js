f8b312e7da0eb802cad2411237867490a95ef557
cerensolpan <cerensolpan@gmail.com>
2021-01-09 16:56:14 +0300

    Refactored homework, created new classes and functions

--- Week_1/index.js
+++ Week_1/index.js
@@ -7,11 +7,16 @@ User = class {
     // Method for adding songs to user playlist
     listenedPlaylist(song) {
         this.playlist.push(song);
-        this.playlist.forEach(printSongNamesToConsole);
     }
-}
-// Print each of the user playlist songs to console
-printSongNamesToConsole = song => console.log(song.songName);
+    //Print method of listening songs to the console
+    listenedSong() {
+        this.playlist.forEach((element) => printListenedSongToConsole(element, this));
+    }
+};
+
+// Print function
+printListenedSongToConsole = (song, user) =>
+    console.log(user.userName + " is listening to " + song.songName);
 
 // Create Genre Class
 Genre = class {
@@ -19,7 +24,17 @@ Genre = class {
         this.genreName = genreName;
         this.songs = [];
     }
-}
+    //Print method of genres songs to the console
+    genresSongs() {
+        this.songs.forEach((element) => printGenreSongsToConsole(element, this));
+    }
+};
+
+// Print function
+printGenreSongsToConsole = (song, genre) =>
+    console.log(
+        song.songName + " is a song in the " + genre.genreName + " genre. "
+    );
 
 // Create Artist Class
 Artist = class {
@@ -27,7 +42,15 @@ Artist = class {
         this.artistName = artistName;
         this.songs = [];
     }
-}
+    //Print method of artist songs to the console
+    artistSongs() {
+        this.songs.forEach((element) => printArtistSongsToConsole(element, this));
+    }
+};
+
+//Print function
+printArtistSongsToConsole = (song, artist) =>
+    console.log(song.songName + " belongs to " + artist.artistName);
 
 // Create Song Class
 Song = class {
@@ -44,8 +67,7 @@ Song = class {
         this.artist = artist;
         artist.songs.push(this);
     }
-
-}
+};
 
 // Create new User objects
 ceren = new User("Ceren");
@@ -53,20 +75,40 @@ yasemin = new User("Yasemin");
 
 // Create new Song object
 lucky = new Song("Lucky");
+giveUp = new Song(" I Won't Give Up");
+wait = new Song("Should I Wait");
 
 // Add song to user playlist
 ceren.listenedPlaylist(lucky);
+ceren.listenedPlaylist(wait);
+yasemin.listenedPlaylist(giveUp);
 
 // Create new Genre object
 pop = new Genre("Pop");
+blues = new Genre("Blues");
 
-// Add song to genres
+// Add songs to genre list
 lucky.addSongToGenreList(pop);
+giveUp.addSongToGenreList(pop);
+wait.addSongToGenreList(blues);
 
 //Create new Artist object
 jason = new Artist("Jason Mraz");
+luther = new Artist("Luther Allison");
 
-// Add song to artist
+// Add songs to artist
 lucky.addSongToArtist(jason);
+giveUp.addSongToArtist(jason);
+wait.addSongToArtist(luther);
 
+// Implement User's listened song method
+ceren.listenedSong();
+yasemin.listenedSong();
 
+//Implement Song's genre method
+pop.genresSongs();
+blues.genresSongs();
+
+//Implement Artist's song method
+jason.artistSongs();
+luther.artistSongs();
