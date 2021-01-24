91092887e9ac71e55424fb99a80da83738768a39
cerensolpan <cerensolpan@gmail.com>
2021-01-24 18:44:15 +0300

    Add artist, genre,song tests and fix user test

--- wtmbjs-6/test/user-test.cjs
+++ wtmbjs-6/test/user-test.cjs
@@ -8,30 +8,39 @@ test('Create new user', async t => {
         name: 'Aslı',
         playlist: []
     }
-    // create user
+    // create user 
     const res = (await request(app)
         .post('/user')
         .send(newUser))
-    // console.log(res);
+
     t.is(res.status, 200)
     t.is(res.body.name, newUser.name)
 
     t.deepEqual(res.body.playlist, newUser.playlist)
-
 })
 
-test('New song released by artist', async t => {
-    t.plan(1)
-    const newSong = {
-        artistId: '60089d768dde461e50cb5e2b',
-        genreId: '600978742ea9694e3d4bbf57',
-        name: 'Diamond',
+
+test('Song is added users playlist', async t => {
+    t.plan(3)
+    const addPlaylist = {
+        userId: '60097df52474f6fbf032d634',
+        songId: '600c6968f2a87e2d8055fbed'
     }
-
-    // create song
+    // console.log(addPlaylist);
     const res = (await request(app)
-        .post('/artist/release')
-        .send(newSong))
+        .post('/user/add-playlist')
+        .send(addPlaylist))
+    console.log(res.body);
+    t.is(res.status, 200)
+    t.is(res.body.userId, addPlaylist.userId)
+    t.is(res.body.songId, addPlaylist.songId)
+})
 
+test('Get list of user', async t => {
+    t.plan(1)
+
+    // get the list of users - render view
+    const res = await request(app).get('/user/all')
+    // checking for server response status success
     t.is(res.status, 200)
 })
